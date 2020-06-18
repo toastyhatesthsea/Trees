000079422d2addf895d3e08828679f34b8c00c9f
toastyhatesthsea <toastyhatesthesea@gmail.com>
2020-06-17 18:27:03 -0700

    Fixed add function

--- src/Trees/StringBinary.java
+++ src/Trees/StringBinary.java
@@ -9,13 +9,58 @@ public class StringBinary
         root = new BTNode<>(null, null, null);
     }
 
+    public void addTwo(String someData)
+    {
+        if (root.getData() == null)
+        {
+            root.setData(someData);
+        } else if (root.isLeaf())
+        {
+            BTNode<String> temp = new BTNode<>(null, null, null);
+            temp.setData(someData);
+            if (someData.compareToIgnoreCase(root.getData()) > 0)
+            {
+                root.setRight(temp);
+            } else
+            {
+                root.setLeft(temp);
+            }
+        } else
+        {
+            BTNode<String> oldRoot = root;
+            if (someData.compareToIgnoreCase(root.getData()) > 0)
+            {
+                if (root.getRight() == null)
+                {
+                    BTNode<String> temp = new BTNode<>(someData, null, null);
+                    oldRoot.setRight(temp);
+                } else
+                {
+                    root = root.getRight();
+                    addTwo(someData);
+                }
+            } else
+            {
+                if (root.getLeft() == null)
+                {
+                    BTNode<String> temp = new BTNode<>(someData, null, null);
+                    oldRoot.setLeft(temp);
+                } else
+                {
+                    root = root.getLeft();
+                    addTwo(someData);
+                }
+            }
+            root = oldRoot;
+        }
+    }
+
     public void add(String someData)
     {
         if (root == null)
         {
 
-        }
-        else if (root.getData() == null)
+        } else if (root.getData() == null)
         {
             root.setData(someData);
         } else if (root.isLeaf())
@@ -68,12 +113,17 @@ class StringBinaryTesters
     {
         StringBinary aTree = new StringBinary();
 
-        aTree.add("c");
+       /* aTree.add("c");
         aTree.add("a");
         aTree.add("b");
         aTree.add("D");
-        aTree.add("e");
+        aTree.add("e");*/
 
+        aTree.addTwo("c");
+        aTree.addTwo("a");
+        aTree.addTwo("b");
+        aTree.addTwo("D");
+        aTree.addTwo("z");
 
     }
 }
